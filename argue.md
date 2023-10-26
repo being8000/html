92a22fdda77a7c9c912bdc5cefb271c4c5c4c02a
being8000 <1041003513@qq.com>
2023-10-26 13:23:24 +0800

    pure javascript implement simple Pie Chart

---
+++ argue.md
@@ -0,0 +1,22 @@
+| word | explaination | degree |
+| - | - | - |
+| squabble | - | - |
+| dispute | - | - |
+| debate | - | - |
+| quarrel | - | - |
+| argue | - | - |
+| quibble | - | - |
+| wrangle | - | - |
+| brawl | - | - |
+
+
+
+Brochure
+buzz
+Canned
+Captive
+Carve on the tombstone
+Cavity, Concavity Dent Depression, Excavate, Hole, Hollow, Indentation, Pit, Recess
+Cesspit`
+Cereal
+Commiserate
---
+++ timing.js
@@ -0,0 +1,18 @@
+function bounce(timeFraction) {
+  for (let a = 0, b = 1; 1; a += b, b /= 2) {
+    if (timeFraction >= (7 - 4 * a) / 11) {
+      return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+    }
+  }
+}
+
+function linear(timeFraction) {
+  return timeFraction;
+}
+
+// accepts a timing function, returns the transformed variant
+function makeEaseOut(timing) {
+  return function (timeFraction) {
+    return 1 - timing(1 - timeFraction);
+  }
+}
